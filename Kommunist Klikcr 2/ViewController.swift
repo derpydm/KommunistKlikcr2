52279c86b8d5f744ef835a3a955b6c30a331375a
Sean Wong <derpydm@gmail.com>
2020-01-10 15:52:53 +0800

    Initial Commit

---
+++ Kommunist Klikcr 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kommunist Klikcr 2
+//
+//  Created by Tinkertanker on 10/1/20.
+//  Copyright © 2020 Tinkertanker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
